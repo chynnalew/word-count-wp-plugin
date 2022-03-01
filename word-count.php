29a01605cf99c779895df8eea4a316fa118560d9
chynnalew <chynnalew@yahoo.com>
2022-02-28 17:58:33 -0800

    link first setting with main html

--- word-count.php
+++ word-count.php
@@ -15,10 +15,14 @@ class WordCountAndTimePlugin {
   }
 
   function settings() {
+    add_settings_section('wcp_first_section', null, null, 'word-count-settings-page',);
     add_settings_field('wcp_location', 'Display Location', array($this, 'locationHTML'), 'word-count-settings-page', 'wcp_first_section');
-    
     register_setting('wordcountplugin', 'wcp_location', array('sanitize_callback' => 'sanitize_text_field', 'default' => '0'));
   }
+
+  function locationHTML() { ?>
+    hello!
+  <?php }
 
   function adminPage() {
     add_options_page("Word Count Settings", 'Word Count', 'manage_options', 'word-count-settings-page', array($this, 'html'));
@@ -27,6 +31,11 @@ class WordCountAndTimePlugin {
   function html() { ?>
     <div class='wrap'>
       <h1> Word Count Settings </h1>
+      <form action="options.php" method="post">
+        <?php
+          do_settings_sections('word-count-settings-page');
+        ?>
+      </form>
     </div>
   <?php }
 
